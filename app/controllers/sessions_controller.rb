55fb87660d3fc5a2195e3dd392f62a2621b04928
Jeff Caldwell <jeff.caldwell@shopify.com>
2017-08-02 11:42:07 -0400

    fixed redirect loops

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,21 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize
+
   def create
     user = User.find_by(name: params[:name])
     if user.try(:authenticate, params[:password])
       session[:user_id] = user.id
-      redirect_to pets_path
+      redirect_to home_index_path
     else
       redirect_to login_url, alert: "Invalid user/password combination"
     end
   end
 
   def new
-    debugger
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to home_index_path, notice: "Logged out"
+    redirect_to login_url, notice: "Logged out"
   end
 end
